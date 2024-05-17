b1b0b2137a628f67e6339cb89eca5b3538c3c021
Dima Galkin <dim.galkin@gmail.com>
2024-05-17 23:19:13 +0100

    Merging branches

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ ThreeDL::CameraController controller (
 ThreeDL::Renderer renderer (controller, 1920, 1080);
 
 ThreeDL::Object plane (
-        "src/A-10.obj",
+        "src/MiG-29.obj",
         ThreeDL::FILEFORMAT::OBJ,
-        "src/A-10.png"
+        "src/MIG-29.png"
 );
 
 ThreeDL::vec3 light_pos1 {plane.centre_.x, plane.centre_.y + 50, plane.centre_.z};
